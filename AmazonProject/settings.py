c682e42a4e6beebb8fb6d7fd0cd47661b6f5a84a
barchh <bworkshimelis1738@gmail.com>
2026-04-21 04:00:00 -0400

    new setting upate555

--- AmazonProject/settings.py
+++ AmazonProject/settings.py
@@ -193,10 +193,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = config('MEDIA_URL', default='/media/')
-if os.environ.get('RENDER') == 'true':
-    MEDIA_ROOT = '/opt/render/project/media'
-else:
-    MEDIA_ROOT = BASE_DIR / 'media'
+# Remove MEDIA_ROOT override when using Backblaze
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 INSTALLED_APPS += ["storages"]
 
@@ -206,8 +204,11 @@ AWS_ACCESS_KEY_ID = config("B2_KEY_ID")
 AWS_SECRET_ACCESS_KEY = config("B2_APP_KEY")
 AWS_STORAGE_BUCKET_NAME = config("B2_BUCKET_NAME")
 AWS_S3_ENDPOINT_URL = "https://s3.us-west-002.backblazeb2.com"
+
+AWS_DEFAULT_ACL = None
 AWS_QUERYSTRING_AUTH = True
 AWS_QUERYSTRING_EXPIRE = 31536000
+
 
 # ---------------------------------------------------------------------
 # Authentication
