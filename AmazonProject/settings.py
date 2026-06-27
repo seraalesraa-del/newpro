fe058300a49a0ce34ec7e811be7fd97a43ed64d6
seraalesraa-del <seraalesraa@gmail.com>
2026-06-27 04:26:58 -0700

    Update settings.py

--- AmazonProject/settings.py
+++ AmazonProject/settings.py
@@ -217,6 +217,9 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
+# Ensure Django uses Backblaze S3 for default storage
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
 
 
 # ---------------------------------------------------------------------
